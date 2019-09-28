e95965ba11df2be53a495b13f006addbe6fbfadc
arnaudsm <arnaudsm66@gmail.com>
2019-09-29 00:57:59 +0200

    benchmark in readme

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Sudoku Solver in Python
 Based on [Peter Norvig's article](https://norvig.com/sudoku.html).
 
+# Performance
+412 sudokus/sec on an `Intel i7-8750H` (single-threaded).
+
 # Dependencies
 - `Python 2.5+`
 
--- main.py
+++ main.py
@@ -9,4 +9,4 @@ solve_all(from_file("data/Sudoku_top95.txt"), "top95", None)
 solve_all(from_file("data/Sudoku_hardest.txt"), "hardest", None)
 
 ## Solve 99 random sudokus
-solve_all([random_puzzle() for _ in range(99)], "aléatoires", 0.01)
+solve_all([random_puzzle() for _ in range(100)], "aléatoires", 0.01)
